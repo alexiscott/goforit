33e75d1ec058c5eea1613c7e583e79384b9ae8c5
Alex Scott <alex@alexscott.net>
2025-10-30 17:30:07 +0100

    Very basic Drag and drop kanban style.
    Simplified.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,36 @@
-function component() {
-    const element = document.createElement('div');
-    element.innerHTML = "Hi folks, I'm Booty!";
-    return element;
-}
+const columns = document.querySelectorAll(".task-column");
 
-document.body.appendChild(component());
+columns.forEach((column) => {
+  column.addEventListener("dragover", (event) => {
+    // Test a custom type we will set later
+    if (event.dataTransfer.types.includes("task")) {
+      event.preventDefault();
+    }
+  });
+});
+
+const tasks = document.querySelectorAll(".task");
+
+tasks.forEach((task) => {
+  task.addEventListener("dragstart", (event) => {
+    task.id = "dragged-task";
+    event.dataTransfer.effectAllowed = "move";
+    // Custom type to identify a task drag
+    event.dataTransfer.setData("task", "");
+  });
+
+  task.addEventListener("dragend", (event) => {
+    task.removeAttribute("id");
+  });
+});
+
+
+columns.forEach((column) => {
+  column.addEventListener("drop", (event) => {
+    event.preventDefault();
+
+    const draggedTask = document.getElementById("dragged-task");
+    draggedTask.remove();
+    column.children[1].appendChild(draggedTask);
+  });
+});
